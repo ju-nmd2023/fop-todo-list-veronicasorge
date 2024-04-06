092123647d4c2fb1e7149bfa94475502c7607e74
veronicasorge <150162661+veronicasorge@users.noreply.github.com>
2024-04-06 14:30:34 +0200

    finally!!!!!!!!!

--- script.js
+++ script.js
@@ -1,27 +1,59 @@
 const inputBox = document.getElementById("input-box");
 const taskList = document.getElementById("task-list");
 
+let myTasks = []; //create an array to store tasks
+
+//load local storage content into the array
+window.addEventListener("load", function loadTasks() {
+  let storedData = localStorage.getItem("myTasks");
+  if (storedData) {
+    myTasks = JSON.parse(storedData);
+  }
+  renderTasks();
+});
+
 function addTask() {
-  if (inputBox.value === "") {
-    alert("Can't submit an empty task!");
+  const taskText = inputBox.value.trim();
+
+  //check if the input is not empty, then add task to the array
+  if (taskText !== "") {
+    myTasks.push({ text: taskText, checked: false });
+    renderTasks();
+    saveTasks();
   } else {
-    let li = document.createElement("li");
-    li.textContent = inputBox.value;
-    li.addEventListener("click", function () {
+    alert("Can't submit an empty task!");
+  }
+  //clear the input box after adding task
+  inputBox.value = "";
+}
+
+function renderTasks() {
+  taskList.innerHTML = "";
+
+  myTasks.forEach(function (task, index) {
+    let newTask = document.createElement("li");
+    newTask.textContent = task.text;
+
+    if (task.checked) {
+      newTask.classList.add("checked");
+    }
+
+    newTask.addEventListener("click", function check() {
+      task.checked = !task.checked;
       this.classList.toggle("checked");
       saveTasks();
     });
 
-    const button = document.createElement("button");
-    button.innerText = "✖︎";
-    button.onclick = removeElement;
-    button.setAttribute("class", "deletebutton");
-    li.appendChild(button);
+    const removeButton = document.createElement("button");
+    removeButton.innerText = "✖︎";
+    removeButton.onclick = function () {
+      removeTask(index);
+    };
+    removeButton.setAttribute("class", "deletebutton");
 
-    taskList.appendChild(li);
-    inputBox.value = "";
-    saveTasks();
-  }
+    newTask.appendChild(removeButton);
+    taskList.appendChild(newTask);
+  });
 }
 
 //add task with enter key as well
@@ -31,17 +63,12 @@ inputBox.addEventListener("keypress", function (event) {
   }
 });
 
+function removeTask(index) {
+  myTasks.splice(index, 1);
+  renderTasks();
+  saveTasks();
+}
+
 function saveTasks() {
-  localStorage.setItem("myTasks", taskList.innerHTML);
-}
-
-function showTasks() {
-  taskList.innerHTML = localStorage.getItem("myTasks");
-}
-showTasks();
-
-function removeElement() {
-  const element = this.parentNode;
-  console.log(element);
-  element.parentNode.removeChild(element);
+  localStorage.setItem("myTasks", JSON.stringify(myTasks));
 }
